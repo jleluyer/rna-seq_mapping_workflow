b6173d7223ffd2c43cd205eaf51155616a4b24f5
jleluyer <jeremy.le.luyer@gmail.com>
2016-05-06 16:51:38 -0400

    changed pathways

--- 00_scripts/colosse_jobs/01_trimmomatic_jobs.sh
+++ 00_scripts/colosse_jobs/01_trimmomatic_jobs.sh
@@ -2,12 +2,12 @@
 
 # launch scripts for Colosse
 
-for file in $(ls 02-data/*.f*q.gz|perl -pe 's/_R[12].f(ast)?q.gz//')
+for file in $(ls 02_data/*.f*q.gz|perl -pe 's/_R[12].f(ast)?q.gz//')
 do
 
 base=$(basename "$file")
 
-	toEval="cat 00-scripts/01-trimmomatic.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00-scripts/colosse_jobs/TRIM_$base.sh
+	toEval="cat 00_scripts/01_trimmomatic.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/colosse_jobs/TRIM_$base.sh
 done
 
  #set working directory
@@ -18,5 +18,5 @@ sed -i "s#__PWD__#$(pwd)#g" 00-scripts/colosse_jobs/TRIM*sh
 
 #Submit jobs
 
-for i in $(ls 00-scripts/colosse_jobs/TRIM*sh); do msub $i; done
+for i in $(ls 00_scripts/colosse_jobs/TRIM*sh); do msub $i; done
 
