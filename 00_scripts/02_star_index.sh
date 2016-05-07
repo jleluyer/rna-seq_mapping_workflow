faf3cfef9a6ae164d25331b66e70084a81d04a09
jleluyer <jeremy.le.luyer@gmail.com>
2016-05-07 12:34:07 -0400

    path corrected

--- 00_scripts/02_star_index.sh
+++ 00_scripts/02_star_index.sh
@@ -9,10 +9,22 @@
 #PBS -l nodes=1:ppn=8
 #PBS -r n
 
+
+
+#log
+#variables
+TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
+SCRIPT=$0
+NAME=$(basename $0)
+LOG_FOLDER="98_log_files"
+cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
+
+#prequis
 module load compilers/gcc/4.8  apps/mugqic_pipeline/2.1.1
 module load mugqic/blat/36
 module load mugqic/star/2.5.0a
 
+#variable
 PWD="__PWD__"
 
 cd $PWD
@@ -25,4 +37,4 @@ STAR --runThreadN 1 \
     	  --genomeFastaFiles  \
           --sjdbOverhang 99 \
           --genomeChrBinNbits 10 \
-          --limitGenomeGenerateRAM 20000000000
+          --limitGenomeGenerateRAM 20000000000 2>&1 | tee 98_log_files/"$TIMESTAMP"_star_index.log 
