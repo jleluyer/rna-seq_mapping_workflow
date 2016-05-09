838c1fd0457d2b8acce37caeb2bd12082acee33b
jleluyer <jeremy.le.luyer@gmail.com>
2016-05-09 13:34:48 -0400

    Update 03_star_mapping_jobs.sh

--- 00_scripts/colosse_jobs/03_star_mapping_jobs.sh
+++ 00_scripts/colosse_jobs/03_star_mapping_jobs.sh
@@ -2,21 +2,21 @@
 
 # launch scripts for Colosse
 
-for file in $(ls 02-data/*paired*.f*q.gz|perl -pe 's/.R[12].paired.fastq.gz//')
+for file in $(ls 03_trimmed/*paired*.f*q.gz|perl -pe 's/.R[12].paired.fastq.gz//')
 do
 
 base=$(basename "$file")
 
-	toEval="cat 00-scripts/03-star_mapping.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00-scripts/colosse_jobs/MAP_$base.sh
+	toEval="cat 00_scripts/03_star_mapping.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/colosse_jobs/MAP_$base.sh
 done
 
  #set working directory
 
 PWD=$(pwd)
 
-sed -i "s#__PWD__#$(pwd)#g" 00-scripts/colosse_jobs/MAP*sh 
+sed -i "s#__PWD__#$(pwd)#g" 00_scripts/colosse_jobs/MAP*sh 
 
 #Submit jobs
 
-for i in $(ls 00-scripts/colosse_jobs/MAP*sh); do msub $i; done
+for i in $(ls 00_scripts/colosse_jobs/MAP*sh); do msub $i; done
 
