1ae84eef6c96618b2b6a99c80e5c5eca59af88d0
jleluyer <jeremy.le.luyer@gmail.com>
2016-05-17 14:01:34 -0400

    added transdecoder, transvestigator and transrate

--- 00_scripts/00_import_tools.sh
+++ 00_scripts/00_import_tools.sh
@@ -7,4 +7,16 @@ module load apps/git/1.8.5.3
 
 cd $(pwd)
 
+
+# clone trinotate
+git clone https://github.com/Trinotate/Trinotate 00_scripts/trinotate_utils
+
+#clone transDecoder
+git clone https://github.com/TransDecoder/TransDecoder 00_scripts/transdecoder_utils
+
+# clone transvestigator
 git clone https://github.com/genomeannotation/transvestigator 00_scripts/transvestigator_utils
+
+#clone transrate
+git clone https://github.com/blahah/transrate 00_scripts/transrate_utils
+
