a2844dd50ea79a6741a04209849850938eb96747
jleluyer <jeremy.le.luyer@gmail.com>
2016-10-10 13:04:20 -0400

    Update 02_star_index.sh

--- 00_scripts/deprecated/02_star_index.sh
+++ 00_scripts/deprecated/02_star_index.sh
@@ -4,7 +4,7 @@
 #PBS -o STAR_index.out
 #PBS -e STAR_index.err
 #PBS -l walltime=24:00:00
-#PBS -M jeremy.le-luyer.1@ulaval.ca
+#PBS -M userid@ulaval.ca
 #PBS -m ea 
 #PBS -l nodes=1:ppn=8
 #PBS -r n
