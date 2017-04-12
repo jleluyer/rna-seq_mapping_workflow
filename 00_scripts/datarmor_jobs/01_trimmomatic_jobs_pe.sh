10bc6804f6fcad14df05bcc07c44d3209085b793
jleluyer <jeremy.le.luyer@gmail.com>
2017-04-11 16:27:59 -1000

    Update 01_trimmomatic_jobs_pe.sh

--- 00_scripts/datarmor_jobs/01_trimmomatic_jobs_pe.sh
+++ 00_scripts/datarmor_jobs/01_trimmomatic_jobs_pe.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Clean up
+rm 00_scripts/datarmor_jobs/TRIM*sh
+
 # launch scripts for Colosse
 for file in $(ls 02_data/*.f*q.gz|perl -pe 's/_[12].f(ast)?q.gz//')
 
 do
 	base=$(basename "$file")
-	toEval="cat 00_scripts/01_trimmomatic_pe.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/datarmor_jobs/TRIM_$base.sh
+	toEval="cat 00_scripts/01_trimmomatic_pe.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/datarmor_jobs/TRIM_$base.sh00_scripts/datarmor_jobs/TRIM_$base.sh
 done
 
 
