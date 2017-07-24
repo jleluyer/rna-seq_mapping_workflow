c25f917541731c7cc332c2303d094f532f0cf080
Jeremy LE LUYER <jleluyer@datarmor3.ib0.ice.ifremer.fr>
2017-07-24 20:05:49 +0000

    added parallel htseq

--- 00_scripts/04_htseq_count.sh
+++ 00_scripts/04_htseq_count.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#PBS -N htseq_index
-#PBS -o htseq_index.err
-#PBS -l walltime=24:00:00
+#PBS -N htseq__BASE__
+#PBS -o htseq__BASE__.err
+#PBS -l walltime=02:00:00
 #PBS -m ea
 #PBS -l ncpus=1
 #PBS -l mem=30g
@@ -18,14 +18,10 @@ cd $PBS_O_WORKDIR
 DATAINPUT="04_mapped"
 DATAOUTPUT="05_count"
 GFF_FOLDER="/home1/datawork/jleluyer/00_ressources/transcriptomes/P_margaritifera"
-GFF_FILE="Trinity.0.5.210717.gff"
+GFF_FILE="Trinity.0.5.210717.gff3"
 
 #launch script
-for i in $(ls 04_mapped/*sorted.bam)
-do
-base="$(basename $i)"
+base=__BASE__
 
-htseq-count --format='bam' --stranded='yes' --order='name' --type='CDS' --idattr='Name' "$DATAINPUT"/"$base" "$GFF_FOLDER"/"$GFF_FILE" \
- "$DATAINPUT"/"$base" "$GFF_FOLDER"/"$GFF_FILE" >> "$DATAOUTPUT"/htseq-count_"$base".txt
+htseq-count -f 'bam' -s 'no' -r 'pos' -t 'CDS' -i 'Name' "$DATAINPUT"/"$base".sorted.bam "$GFF_FOLDER"/"$GFF_FILE" >>"$DATAOUTPUT"/htseq-count_"$base".txt
 
-done 2>&1 | tee 98_log_files/"$TIMESTAMP"_htseq.log
---
+++ 00_scripts/datarmor_jobs/04_hsteq_count_jobs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# Clean past jobs
+
+rm 00_scripts/datarmor_jobs/HTSQ_*sh
+
+for i in $(ls 04_mapped/*sorted.bam|sed 's/.sorted.bam//g')
+do
+base="$(basename $i)"
+
+	toEval="cat 00_scripts/04_htseq_count.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/datarmor_jobs/HTSQ_$base.sh
+done
+
+
+#Submit jobs
+for i in $(ls 00_scripts/datarmor_jobs/HTSQ*sh); do qsub $i; done
+
+
