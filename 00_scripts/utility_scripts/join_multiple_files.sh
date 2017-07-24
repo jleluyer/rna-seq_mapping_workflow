d3048b5f7787eeed6b2d94789ed8ce17d6828c6c
Jeremy LE LUYER <jleluyer@datarmor3.ib0.ice.ifremer.fr>
2017-07-24 20:18:56 +0000

    added join multiple

---
+++ 00_scripts/utility_scripts/join_multiple_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#usage: join_multiple_files.sh file*
+
+if [[ $# -ge 2 ]]; then
+    function __r {
+        if [[ $# -gt 1 ]]; then
+            exec join - "$1" | __r "${@:2}"
+        else
+            exec join - "$1"
+        fi
+    }
+
+    __r "${@:2}" < "$1"
+fi
