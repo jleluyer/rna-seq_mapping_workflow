6526be8350462b69295fedc1ad0de720230d0461
jleluyer <jeremy.le.luyer@gmail.com>
2018-03-01 07:32:29 -1000

    Update 05b_junctionseq_count_filter.sh

--- 00_scripts/05b_junctionseq_count_filter.sh
+++ 00_scripts/05b_junctionseq_count_filter.sh
@@ -11,11 +11,11 @@
 cd $PBS_O_WORKDIR
 
 
-java -jar -Xmx20g /home1/datahome/jleluyer/softwares/QoRTs-STABLE.jar \
+java -jar -Xmx20g QoRTs-STABLE.jar \
 		mergeNovelSplices \
 		--minCount 6 \
 		--stranded \
 		05_count_jctseq/ \
 		01_info_files/decoder.file.txt \
-		/home1/datawork/jleluyer/01_projects/transcriptome_assembly/gawn/04_annotation/indexed_genome.gtf \
+		indexed_genome.gtf \
 		05_count_jctseq/
