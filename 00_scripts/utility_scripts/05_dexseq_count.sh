6dec8c5c62aefc5a21f669f23b5da2a2c3887a0c
jleluyer <jeremy.le.luyer@gmail.com>
2018-03-01 07:33:38 -1000

    Update 05_dexseq_count.sh

--- 00_scripts/utility_scripts/05_dexseq_count.sh
+++ 00_scripts/utility_scripts/05_dexseq_count.sh
@@ -13,4 +13,4 @@ cd $PBS_O_WORKDIR
 
 base=__BASE__
 
-source activate test && python /home1/datahome/jleluyer/R/x86_64-pc-linux-gnu-library/3.3/DEXSeq/python_scripts/dexseq_count.py -f bam /home1/datawork/jleluyer/01_projects/transcriptome_assembly/gawn/04_annotation/indexed_genome.gff 04_mapped/"$base".sorted.genome.bam 05_count_dexseq/"$base".dexseq.txt
+source activate test && python /dexseq_count.py -f bam /indexed_genome.gff 04_mapped/"$base".sorted.genome.bam 05_count_dexseq/"$base".dexseq.txt
